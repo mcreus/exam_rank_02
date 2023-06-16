599c681eb8b295361f8103b0ba4ecc95a5c64113
Maxime Creus <mcreus@c2r1p1.42perpignan.fr>
2023-06-16 14:04:59 +0200

    rank02

---
+++ LEVEL02/rstr_capitalizer/rstr_capitalizer.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rstr_capitalizer.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcreus <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/16 13:26:47 by mcreus            #+#    #+#             */
+/*   Updated: 2023/06/16 13:27:00 by mcreus           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void    rstr_capitalizer(char *str)
+{
+    int i = 0;
+
+    while (str[i])
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] += 32;
+        if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' \
+                    || str[i + 1] == '\t' || str[i + 1] == '\0'))
+            str[i] -= 32;
+        write(1, &str[i++], 1);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int i;
+
+    if (argc == 1)
+        write(1, "\n", 1);
+    else
+    {
+        i = 1;
+        while (i < argc)
+        {
+            rstr_capitalizer(argv[i]);
+            write(1, "\n", 1);
+            i += 1;
+        }
+    }
+    return (0);
+}
---
+++ LEVEL03/fprime/fprime.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fprime.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcreus <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/16 13:27:30 by mcreus            #+#    #+#             */
+/*   Updated: 2023/06/16 13:27:39 by mcreus           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	number;
+
+	if (argc == 2)
+	{
+		i = 1;
+		number = atoi(argv[1]);
+		if (number == 1)
+			printf("1");
+		while (number >= ++i)
+		{
+			if (number % i == 0)
+			{
+				printf("%d", i);
+				if (number == i)
+					break ;
+				printf("*");
+				number /= i;
+				i = 1;
+			}
+		}
+	}
+	printf("\n");
+	return (0);
+}
